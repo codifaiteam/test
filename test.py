8e086fb7cbdb6bc28f03022d260dabcc34eb4404
codifaiteam <codifaiteam@gmail.com>
2024-08-12 22:35:42 +0330

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ import numpy as np
 import cv2
 from PIL import Image
 
+### read image base on image path
 img = cv2.imread("test.jpg")
+
+#### get shape of image
 h,w = img.shape[:2]
 
+### show image with pil format
 Image.fromarray(img).show()
 
